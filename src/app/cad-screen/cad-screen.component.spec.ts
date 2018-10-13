d2aecf08bde1b05f4a14b5c09a956e84d8754c6e
Huan LI <zixia@zixia.net>
2018-10-13 23:22:35 +0800

    linting

--- src/app/cad-screen/cad-screen.component.spec.ts
+++ src/app/cad-screen/cad-screen.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { CadScreenComponent } from './cad-screen.component';
+import { CadScreenComponent } from './cad-screen.component'
 
 describe('CadScreenComponent', () => {
-  let component: CadScreenComponent;
-  let fixture: ComponentFixture<CadScreenComponent>;
+  let component: CadScreenComponent
+  let fixture: ComponentFixture<CadScreenComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CadScreenComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CadScreenComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(CadScreenComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
