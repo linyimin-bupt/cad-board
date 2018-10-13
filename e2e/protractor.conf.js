feb19e9a883bd4987271ec9c7930fd99e9b1bcb2
Huan LI <zixia@zixia.net>
2018-10-13 21:39:10 +0800

    headless for e2e testing

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -3,13 +3,23 @@
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
+// https://medium.com/@rogeriopvl/automating-karma-and-headless-chrome-with-puppetteer-51ce8f6a78b0
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': [
+        '--no-sandbox',
+        '--disable-gpu',
+        '--headless'
+      ]
+    },
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
