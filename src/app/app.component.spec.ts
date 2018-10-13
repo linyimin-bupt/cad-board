19d5dba3315237be071b635be9a26e8748319119
Huan LI <zixia@zixia.net>
2018-10-13 22:22:36 +0800

    fix unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,12 @@ describe('AppComponent', () => {
   it(`should have as title 'cad-screen'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
-    expect(app.title).toEqual('cad-screen')
+    expect(app.title).toContain('cad-screen')
   }))
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
     const compiled = fixture.debugElement.nativeElement
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cad-screen!')
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cad-screen')
   }))
 })
