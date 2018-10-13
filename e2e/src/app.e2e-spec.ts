62bae9991f8e7d175bd3478a1aa70d0c4a5324aa
Huan LI <zixia@zixia.net>
2018-10-13 22:23:31 +0800

    fix e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo()
-    expect(page.getParagraphText()).toEqual('Welcome to cad-screen!')
+    expect(page.getParagraphText()).toContain('Welcome to cad-screen')
   })
 })
