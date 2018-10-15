a7de08f556c4679d5a081866fccec58a11281657
Huan LI <zixia@zixia.net>
2018-10-15 20:45:50 +0800

    work around with @angular/fire with zone (#1)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,14 +27,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   }))
 
-  it(`should have as title 'cad-screen'`, fakeAsync (() => {
+  it(`should have as title 'cad-screen'`, async (() => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
     expect(app.title).toContain('CAD')
   }))
 
-  it('should render title in a h1 tag', fakeAsync(() => {
+  it('should render title in a h1 tag', (() => {
     const fixture = TestBed.createComponent(AppComponent)
+
     fixture.detectChanges()
     const compiled = fixture.debugElement.nativeElement
     expect(compiled.querySelector('h1').textContent).toContain('CAD')
