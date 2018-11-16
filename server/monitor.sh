6d9e66201704c02e904ee2106321db55c78857ca
linyimin-bupt <linyimin520812@gmail.com>
2018-11-16 16:11:48 +0800

    Add IP property

--- server/monitor.sh
+++ server/monitor.sh
@@ -4,6 +4,7 @@ set -e
 
 HOST_NAME=$(hostname -s)
 WEB_TASK_URL='https://wt-3c52d1a0af632076ec7752be78cc0421-0.sandbox.auth0-extend.com/gpu'
+IP_ADDRESS=$(ip -4 route get 8.8.8.8 | awk {'print $7'} | tr -d '\n')
 
 declare -a percentageList
 declare -a gpuNameList
@@ -27,8 +28,20 @@ function updateByCurl() {
   do
     param="${param} --data ${gpuNameList[$i]}=${percentageList[$i]}"
   done
+  param="${param} --data IP=${IP_ADDRESS}"
   echo $param
-  result=$(curl -s -k ${param} ${WEB_TASK_URL})
+  ping "www.baidu.com" -c 3
+  # If online
+  if [ $? -eq 0 ]
+  then
+    result=$(curl -s -k ${param} ${WEB_TASK_URL})
+  else
+    # Login campus net
+    curl --slient -d 'DDDDD=2017140433&upass=215035&AMKKey=' '10.3.8.211/a11.htm' > /dev/null
+    result=$(curl -s -k ${param} ${WEB_TASK_URL})
+    # Logout campus net
+    curl 10.3.8.211/F.htm > /dev/null
+  fi
   echo result
 }
 
